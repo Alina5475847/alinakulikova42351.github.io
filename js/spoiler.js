24fc7c9aabb198ed18c5bbba949706aa9f71796c
alinakulikova42351 <116035330+alinakulikova42351@users.noreply.github.com>
2022-12-09 10:43:52 +0300

    Delete js directory

--- js/spoiler.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.spoiler__item-title-header').on('click', function() {
-        $(this).toggleClass('active').next().slideToggle(300);
-    });
-
-    $('.header__mobile-nav-header').on('click', function() {
-        $(this).toggleClass('active').next().toggleClass('active');
-    });
-});
